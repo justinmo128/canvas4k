5dbcd73102aa7b8a705a0c87aa3871febd655c5b
justinmo128 <112972855+justinmo128@users.noreply.github.com>
2022-11-11 19:47:53 -0700

    props to derek

--- songfunctions.js
+++ songfunctions.js
@@ -1,8 +1,8 @@
 class Song {
-    constructor(bpm, songoffset, songsrc) {
+    constructor(bpm, offset, songsrc) {
         this.bpm = bpm;
         this.crotchet = 60000 / bpm, // How long a beat is in ms
-        this.songoffset = songoffset, // Length of the beginning of the sound file (where metadata is stored) in ms
+        this.songoffset = offset, // Length of the beginning of the sound file (where metadata is stored) in ms
         this.starttime;
         this.songposition = currentTime - (this.starttime + this.songoffset); // Song position in ms
         this.audio = new Audio(`songs/${songsrc}`);
@@ -10,8 +10,26 @@ class Song {
     }
 }
 
+// Global variables
 let lastbeat;
-const GHOST = new Song(220, 8, "Camellia - GHOST.ogg");
+let song;
+let notes;
+const tickSound = new Audio('snd/tick.mp3');
+tickSound.volume = 0.4;
+
+// JSON parser thing
+const xmlhttp = new XMLHttpRequest();
+const GHOSTUrl = "songs/GHOST.json";
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    song = JSON.parse(this.responseText);
+  }
+};
+xmlhttp.open("GET", GHOSTUrl, false);
+xmlhttp.send();
+
+// Make a new song object
+const GHOST = new Song(song.bpm, song.offset, song.music);
 
 function startSong() {
     lastbeat = 0;
@@ -19,131 +37,48 @@ function startSong() {
     GHOST.starttime = performance.now();
 }
 
-const tickSound = new Audio('snd/tick.mp3'); //temporary
-tickSound.volume = 0.4;
 function updateSong() {
     GHOST.songposition = (currentTime - (GHOST.starttime + GHOST.songoffset));
     if (GHOST.songposition > lastbeat + GHOST.crotchet) {
         lastbeat = lastbeat + GHOST.crotchet;
         console.log("Beat occurred!");
-        tickSound.play(); //temporary
+        tickSound.play();
     }
 }
 
 function calcNotes() {
-    for (let i = 0; i < notes.length; i++) {
-        // Calculate y
-        notes[i].distanceFromReceptor = ((notes[i].measure - 1) * 4 * GHOST.crotchet - GHOST.songposition);
-        notes[i].y = 400 - notes[i].distanceFromReceptor;
-        // Make an array
-        const splitNotes = Array.from(notes[i].notes, Number);
-        // Send to drawNotes function
-        drawNotes(notes[i].y, splitNotes);
-    }
-}
-
-function drawNotes(y, splitNotes) {
-    if (y < 480 && y > -50) {
-        if (splitNotes[0] === 1) {
-            ctx.fillStyle = "purple";
-            ctx.fillRect(204, y, 50, 50);
-        }
-        if (splitNotes[1] === 1) {
-            ctx.fillStyle = "cyan";
-            ctx.fillRect(264, y, 50, 50);
-        }
-        if (splitNotes[2] === 1) {
-            ctx.fillStyle = "lime";
-            ctx.fillRect(324, y, 50, 50);
-        }
-        if (splitNotes[3] === 1) {
-            ctx.fillStyle = "red";
-            ctx.fillRect(384, y, 50, 50);
+    // song.notes.length is the amount of measures
+    for (let i = 0; i < song.notes.length; i++) { 
+        // song.notes[i].length is the snap of the measure (eg. 4 is 4th snap)
+        for (let j = 0; j < song.notes[i].length; j++) { 
+            let noteMeasure = (i + j / song.notes[i].length);
+            let noteTime = noteMeasure * 4 * GHOST.crotchet;
+            if (GHOST.songposition - GHOST.crotchet < noteTime) {
+                // Calculate y
+                // noteTime - GHOST.songposition is the distance from the receptor
+                y = 400 - (noteTime - GHOST.songposition); 
+                // Send to drawNotes function
+                drawNotes(song.notes[i][j], y);
+            };
         }
     }
 }
 
-// To do: use base 2 number instead of string
-let notes = [
-    {
-        measure: 9,
-        notes: "1000",
-        // notes2:
-        // [1000,
-        // 0000,
-        // 0100,
-        // 0010,]
-    },
-    {
-        measure: 9.5,
-        notes: "0100",
-    },
-    {
-        measure: 9.75,
-        notes: "0010",
-    },
-    {
-        measure: 10.5,
-        notes: "0001",
-    },
-    {
-        measure: 11,
-        notes: "0100",
-    },
-    {
-        measure: 11.5,
-        notes: "0001",
-    },
-    {
-        measure: 11.5,
-        notes: "1000",
-    },
-    {
-        measure: 12.5,
-        notes: "0100",
-    },
-    {
-        measure: 13,
-        notes: "0010",
-    },
-    {
-        measure: 13.5,
-        notes: "0100",
-    },
-    {
-        measure: 13.75,
-        notes: "1000",
-    },
-    {
-        measure: 14.5,
-        notes: "0001",
-    },
-    {
-        measure: 15,
-        notes: "0100",
-    },
-    {
-        measure: 15.5,
-        notes: "0010",
-    },
-    {
-        measure: 15.75,
-        notes: "1000",
-    },
-    {
-        measure: 16.5,
-        notes: "0001",
-    },
-    {
-        measure: 17,
-        notes: "0010",
-    },
-    {
-        measure: 18,
-        notes: "0001",
-    },
-    {
-        measure: 19,
-        notes: "1000",
-    },
-]
+function drawNotes(notes, y) {
+    if (notes.charAt(0) == 1) {
+        ctx.fillStyle = "purple";
+        ctx.fillRect(204, y, 50, 50);
+    }
+    if (notes.charAt(1) == 1) {
+        ctx.fillStyle = "cyan";
+        ctx.fillRect(264, y, 50, 50);
+    }
+    if (notes.charAt(2) == 1) {
+        ctx.fillStyle = "lime";
+        ctx.fillRect(324, y, 50, 50);
+    }
+    if (notes.charAt(3) == 1) {
+        ctx.fillStyle = "red";
+        ctx.fillRect(384, y, 50, 50);
+    }
+}

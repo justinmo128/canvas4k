ffbfb8ff0e19802ca6ded4d2d46e4e104e5f37b5
justinmo128 <j.mo1@share.epsb.ca>
2022-11-24 10:30:27 -0700

    make holds visuals

--- songfunctions.js
+++ songfunctions.js
@@ -27,6 +27,7 @@ class Song {
             ok: 0,
             notgood: 0
         }
+        lastJudgment = "";
         currentSong = song[mainMenuSelect]
         lastbeat = 0;
         this.audio.pause();
@@ -99,23 +100,51 @@ class Note {
 }
 
 class Hold {
-    constructor(dir, start, end) {
+    constructor(dir, start, startMeasure, startSnap) {
         this.dir = dir; // 0 - left, 1 - down, 2 - up, 3 - right
         this.start = start;
-        this.end = end;
         this.isHit = false;
+        this.fullyHeld = false;
         this.startY;
         this.endY;
+        this.tailLength;
+        this.startMeasure = startMeasure;
+        this.startSnap = startSnap;
+        this.end;
+
+        let amountMeasures = currentSong.notes.length;
+        findEnd:
+        for (let i = startMeasure; i < amountMeasures; i++) {
+            let snap = currentSong.notes[i].length;
+            if (i === startMeasure) {
+                for (let j = startSnap; j < snap; j++) { 
+                    if (currentSong.notes[i][j].charAt(this.dir) == 3) {
+                        this.end = (i + j / snap) * 4 * currentSong.crotchet;
+                        console.log(this.start, this.end, this.dir)
+                        break findEnd;
+                    }
+                }
+            } else {
+                if (currentSong.notes[i][j].charAt(this.dir) == 3) {
+                    this.end = (i + j / snap) * 4 * currentSong.crotchet;
+                    console.log(this.start, this.end, this.dir)
+                    break findEnd;
+                }
+            }
+        };
+        
     }
     update() {
         // Calculate y
         // noteTime - song.songposition is the distance from the receptor
         if (downscroll) {
             this.startY = 400 - ((this.start - currentSong.songposition) * (scrollSpeed / 100) + visualOffset); 
-            this.endY = 400 - ((this.end - currentSong.songposition) * (scrollSpeed / 100) + visualOffset);
+            this.endY = 400 - ((this.end - currentSong.songposition) * (scrollSpeed / 100) + visualOffset); 
+            this.tailLength = this.startY - this.endY;
         } else {
             this.startY = ((this.start - currentSong.songposition) * (scrollSpeed / 100) + visualOffset);
             this.endY = ((this.end - currentSong.songposition) * (scrollSpeed / 100) + visualOffset);
+            this.tailLength = this.startY - this.endY + 50;
         }
         // Check if player missed the note
         if (currentSong.songposition >= this.time + 180 && !this.isHit) {
@@ -126,27 +155,36 @@ class Hold {
         }
     }
     draw() {
+        // Draw notes
         if (this.isHit === false) {
             if (this.dir == 0) {
                 ctx.fillStyle = "purple";
                 ctx.fillRect(204, this.startY, 50, 50);
-                ctx.fillStyle = "gray";
-                ctx.fillRect(204, this.endY, 50, 50);
             } else if (this.dir == 1) {
                 ctx.fillStyle = "cyan";
                 ctx.fillRect(264, this.startY, 50, 50);
-                ctx.fillStyle = "gray";
-                ctx.fillRect(264, this.endY, 50, 50);
             } else if (this.dir == 2) {
                 ctx.fillStyle = "lime";
                 ctx.fillRect(324, this.startY, 50, 50);
-                ctx.fillStyle = "gray";
-                ctx.fillRect(324, this.endY, 50, 50);
             } else if (this.dir == 3) {
                 ctx.fillStyle = "red";
                 ctx.fillRect(384, this.startY, 50, 50);
+            }
+        }
+        // Draw end tails
+        if (this.fullyHeld === false) {
+            if (this.dir == 0) {
                 ctx.fillStyle = "gray";
-                ctx.fillRect(384, this.endY, 50, 50);
+                ctx.fillRect(209, this.endY, 40, this.tailLength);
+            } else if (this.dir == 1) {
+                ctx.fillStyle = "gray";
+                ctx.fillRect(269, this.endY, 40, this.tailLength);
+            } else if (this.dir == 2) {
+                ctx.fillStyle = "gray";
+                ctx.fillRect(329, this.endY, 40, this.tailLength);
+            } else if (this.dir == 3) {
+                ctx.fillStyle = "gray";
+                ctx.fillRect(389, this.endY, 40, this.tailLength);
             }
         }
     }
@@ -162,19 +200,42 @@ let holds = [];
 
 function createNotes() {
     gameState = "loadingNotes";
+    createStandardNotes();
+    createHolds();
+    gameState = "gameLoop";
+}
+
+function createStandardNotes() {
     let amountMeasures = currentSong.notes.length;
-    let noteIndex = 0;
+    let index = 0;
     for (let i = 0; i < amountMeasures; i++) {
         let snap = currentSong.notes[i].length;
         for (let j = 0; j < snap; j++) { 
             for (let k = 0; k < 4; k++) {
                 if (currentSong.notes[i][j].charAt(k) == 1) {
                     let noteTime = (i + j / snap) * 4 * currentSong.crotchet;
-                    notes[noteIndex] = new Note(k, noteTime);
-                    noteIndex++;
+                    notes[index] = new Note(k, noteTime);
+                    index++;
                 }
             }
         }
     }
-    gameState = "gameLoop";
+}
+
+function createHolds() {
+    let amountMeasures = currentSong.notes.length;
+    let index = 0;
+    let start;
+    for (let i = 0; i < amountMeasures; i++) {
+        let snap = currentSong.notes[i].length;
+        for (let j = 0; j < snap; j++) { 
+            for (let k = 0; k < 4; k++) {
+                if (currentSong.notes[i][j].charAt(k) == 2) {
+                    start = (i + j / snap) * 4 * currentSong.crotchet;
+                    holds[index] = new Hold(k, start, i, j);
+                    index++;
+                }
+            }
+        }
+    }
 }
